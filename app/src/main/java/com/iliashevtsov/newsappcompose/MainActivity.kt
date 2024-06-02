7cdd746979fd2c7c3b982ea8289ad6304100f716
ilya-shevtsov <Dayzilya@gmail.com>
2024-06-02 14:47:16 +0400

    Initial commit

---
+++ app/src/main/java/com/iliashevtsov/newsappcompose/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.iliashevtsov.newsappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.iliashevtsov.newsappcompose.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
