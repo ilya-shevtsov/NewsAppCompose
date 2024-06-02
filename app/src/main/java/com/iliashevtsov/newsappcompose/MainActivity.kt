12f591db2d51da4099f4a8618b32ee1a61a3f1f4
ilya-shevtsov <Dayzilya@gmail.com>
2024-06-02 16:11:24 +0400

    Added OnBoardingScreen Buttons

--- app/src/main/java/com/iliashevtsov/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/iliashevtsov/newsappcompose/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.iliashevtsov.newsappcompose.presentation.onboarding.OnBoardingScreen
 import com.iliashevtsov.newsappcompose.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
             NewsAppTheme {
