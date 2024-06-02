28e315b3ac1c31b63863d1807af471dce8914337
ilya-shevtsov <Dayzilya@gmail.com>
2024-06-02 15:51:36 +0400

    Added OnBoardingScreen

--- app/src/main/java/com/iliashevtsov/newsappcompose/MainActivity.kt
+++ app/src/main/java/com/iliashevtsov/newsappcompose/MainActivity.kt
@@ -3,7 +3,12 @@ package com.iliashevtsov.newsappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.iliashevtsov.newsappcompose.presentation.onboarding.OnBoardingScreen
 import com.iliashevtsov.newsappcompose.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +17,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
+                    OnBoardingScreen()
+                }
             }
         }
     }
